f5e652ed2740d8a83a83960f44dbf379c7bfd46c
Micheal Yao <kevinman003@gmail.com>
2025-02-04 14:46:54 -0800

    Get user from db

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,16 +1,20 @@
 import { Request, Response, type Express } from 'express';
-import { addUser } from '../services/UserService';
+import { addUser, getUser } from '../services/UserService';
 
 const UserController = (app: Express) => {
   const addUserHandler = async (req: Request, res: Response) => {
-    const { userId } = req.params;
+    const { userId, email } = req.body;
     if (!userId) {
       res.status(400).json({ error: 'userId is missing' });
       return;
     }
+    if (!email) {
+      res.status(400).json({ error: 'email is missing' });
+      return;
+    }
 
     try {
-      await addUser(userId);
+      await addUser(userId, email);
       res.status(200).json(userId);
     } catch (error) {
       res.status(500).json({ error });
@@ -18,7 +22,24 @@ const UserController = (app: Express) => {
     }
   };
 
-  app.post('/add-user/:userId', addUserHandler);
+  const getUserHandler = async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    if (!userId) {
+      res.status(400).json({ error: 'userId is missing' });
+      return;
+    }
+
+    try {
+      const user = await getUser(userId);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error });
+      return;
+    }
+  };
+
+  app.post('/add-user/', addUserHandler);
+  app.get('/get-user/:userId', getUserHandler);
 };
 
 export default UserController;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -4,15 +4,17 @@ import AwsService from './AwsService';
 
 interface UserDBO extends DBO {
   isPaid: boolean;
+  email: string;
 }
 
-export const addUser = async (userId: string) => {
-  const awsService = AwsService();
+const awsService = AwsService();
 
+export const addUser = async (userId: string, email: string) => {
   const user: UserDBO = {
     PK: `${consts.USER_PK_PREFIX}${userId}`,
     SK: 'METADATA',
     isPaid: false,
+    email,
   };
 
   const userParams = {
@@ -26,4 +28,17 @@ export const addUser = async (userId: string) => {
   } catch (err) {
     throw err;
   }
+};
+
+export const getUser = async (userId: string) => {
+  const userParams = {
+    TableName: consts.IMAGE_DATA_DB_NAME,
+    KeyConditionExpression: 'PK = :pk AND SK = :sk',
+    ExpressionAttributeValues: {
+      ':pk': `${consts.USER_PK_PREFIX}${userId}`,
+      ':sk': consts.USER_SK_PREFIX,
+    },
+  };
+
+  return awsService.query(userId, userParams);
 };
